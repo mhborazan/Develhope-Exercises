c8cf8a4000bddca904bde75c1d8cc79a9205c992
Mustafa Hakan Borazan <borazanmh@outlook.com>
2023-06-27 17:08:39 +0300

    react excercise 3

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -4,12 +4,17 @@ import Seperator from './Seperator'
 import Hello from './excercises/excercise-1/Hello'
 import Message from './excercises/excercise-1/Message'
 import Welcome from './excercises/excercise-2/Welcome'
+import Welcome2 from './excercises/excercise-3/Welcome2'
 
 function App() {
   
 
   return (
     <>
+
+      <Seperator id="3">
+        <Welcome2/>
+      </Seperator>
 
       <Seperator id="2">
         <Welcome name="Hakan" />
---
+++ react/src/excercises/excercise-3/Welcome2.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Welcome2(props) {
+  return (
+    <p>Welcome {props.name || "Mustafa"}</p>
+  )
+}
