79b5358c167fac654815a7ffa6ff8631b5bb9419
Mustafa Hakan Borazan <borazanmh@outlook.com>
2023-06-27 15:54:27 +0300

    react-excercises-1

---
+++ react/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Hello from './excercises/excercise-1/Hello'
+import Message from './excercises/excercise-1/Message'
+
+function App() {
+  
+
+  return (
+    <>
+      <Hello />
+      <Message />
+    </>
+  )
+}
+
+export default App
